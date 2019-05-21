326bd8780538c5b1384a2c50d91590c9231af725
Tito Mordeccai <tito@solutech.co.ke>
2019-05-21 12:51:45 +0300

    Removed compiled code

--- src/app/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const event_emitter_1 = __importDefault(require("event-emitter"));
-const event = event_emitter_1.default();
-//event.on('new:user', UserListener.register);
-exports.default = event;
